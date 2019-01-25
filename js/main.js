6e43fba11a4147fefda9387b4df5ab9f84cd0d5b
Kevin Cornea <kpcornea@gmail.com>
2019-01-25 00:58:45 -0800

    Import files from portfolio-site repo

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// this is a notes file for now
+
+// starter outline
+// top navbar
+// jumbotron with background video play on land like codecademy intensives. name/logo/profile pic opacity and movement animation, probably slight movement upwards. after get basics down, try to implement the react to mouse thing where it moves slightly in reaction to mouse movement. might be able to do that with vanilla JS, whichever makes more sense. not sure what to put after jumbotron, maybe projects i've made.
+
+// review proper/conventional html class names eg jumbotron or main or what? is there a better semantic tag for main content/other areas?
+// oh yeah section tag.
+// header tag? maybe review notes i got from ashwin or whoever PM
+// separate class outside of container for title space / nav? i think so cuz that way i can get effect of never ending nav section
+// maybe use less/sass just for the upgrades like mixins etc. would be a bit easier but i also want to get a working version up asap.
+
+// X change to slightly fatter more modern font. could be a bit better but good enough for now
+// from after changed bg to stars 2, from here i think Xchange bg to video bg i want, then Xupdate fonts and Xadd animation, don't forget X media queries for different screen sizes, then fancier mouse interactivity if time / in future
+// dust particle vid is slightly cut off currently. overall looks pretty cool though.
+// Xedit pic of me to be circular and right size, same for github icon. probably make all links glow/at least change color on hover. an animation would be nice though.
+// X add animations on landing
+// maybe different font for header and links/tagline X i think fine for now
+// might ex the nav X for now since nowhere to navigate to right now
+// maybe splash of color as glow background to pro pic, maybe also github icon
+// maybe space out items a little bit more. looking pretty good now though.
+// make dust vid stretched to full screen if possible..making a lot of trouble for me. will come back to it if time, animations more important, and it's really fine/good as is, just would be nice to cover last bottom section
+// X probs cover for different browsers with animation and possibly check with multiple browsers, yes i think do this
+// X think ex the color circles for now
+// background pic looks quite scrunched on mobile. might be ok for now.
